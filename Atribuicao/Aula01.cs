c6d52d7395029a30084ae809fe2067f73fbc7c0e
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-04-02 11:33:05 -0300

    Atribuicao

---
+++ Atribuicao/Aula01.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace DevSuperior {
+
+    public class Aula02 {
+
+        public static void Main(String[] args) {
+
+            float x = 4.5f;
+
+            double y = x;
+
+            float b = 45.1F;
+
+            double a = (float)b;
+
+            Console.WriteLine(y);
+        }
+    }
+}
