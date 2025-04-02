6f1f9fe5d3423e07f994f048ed5a70437a2acc1f
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-04-02 11:39:18 -0300

    Condicional simples

---
+++ Condicional/Aula01.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace DevSuperior {
+
+    public class Aula01 {
+
+        public static void Main(String[] args) {
+
+            int x = 10;
+
+            Console.WriteLine("Bom dia!");
+
+            if(x > 5) {
+
+                Console.WriteLine("Boa noite!");
+            }
+
+            Console.WriteLine("Boa noite!");
+        }
+    }
+}
