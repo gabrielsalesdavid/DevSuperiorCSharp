e75c0830473007735562ceb226fce82f29eb7555
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-04-02 12:04:11 -0300

    For Each

---
+++ foreach/Aula01.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Globalization;
+
+namespace DevSuperior {
+    class Aula01 {
+        static void Main(string[] args) {
+
+            string[] vect = new string[] { "Maria", "Alex", "Bob" };
+
+            for(int i = 0; i < vect.Length; i++) {
+
+                Console.WriteLine(vect[i]);
+            }
+
+            Console.WriteLine("-----------------------------");
+
+            foreach(string obj in vect) {
+
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
